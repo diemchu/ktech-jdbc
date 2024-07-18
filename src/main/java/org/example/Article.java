e2fefd6f98a2a3a894fdf3568151b4a7e6f7d04c
CHU DUC DIEM <112887374+diemchu@users.noreply.github.com>
2024-07-18 10:08:28 +0900

    add files

---
+++ src/main/java/org/example/Article.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Article
+{
+    private long id;
+    private  String title;
+    private String content;
+
+    public Article(){}
+
+    //개시글 추가할 때
+    public Article(String title,String content){}
+
+    //데이터 불러올 떄 쓸 생성자
+    public Article(long id ,String title,String content){
+        this.id = id;
+        this.title =title;
+        this.content = content;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
